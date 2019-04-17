b2a2861dd9f8d13e55fbf380def4a29be23b1fcf
Yonguk Lee <qjrmsktso2@gmail.com>
2019-04-17 13:04:54 +0900

    add ppt. solve 4

--- 4_20171667.py
+++ 4_20171667.py
@@ -1,6 +1,16 @@
-f = open("p2_data.txt", 'r')
+print('--------------------------')
+print("< 학생 성적 >")
+print('--------------------------')
+
+scores = []
+f = open("p4_data.txt", 'r')
 for line in f:
-    a, b = line.split()
-    a = float(a)
-    b = float(b)
-    print('{:.2f} + {:.2f} = {:.2f}'.format(a, b, a+b))
+    id, s1, s2, s3, s4 = line.split()
+    scores.append([int(id), int(s1), int(s2), int(s3), int(s4)])
+    print(int(id), scores[-1][1:])
+    
+print('--------------------------')
+print("< 평균 >")
+print('--------------------------')
+for line in scores:
+    print('{} - {:.2f}'.format(line[0], (sum(line[1:]) - min(line[1:]))/3))
